44c565e9d15fb7fc4b151964469e281a1383dff7
baylayhoof <baylayhoof@live.dk>
2017-01-09 14:08:57 +0100

    tom

--- test.js
+++
@@ -1,8 +0,0 @@
-function init() {
-	var touchzone = document.getElementById("touchzone");
-	touchzone.addEventListener("touchstart", touchHandler, false);
-}
-
-function touchHandler(event) {
-	document.getElementById("koordinater").innerHTML = 'x: ' + event.touches[0].pageX + ', y: ' + event.touches[0].pageY;
-}
